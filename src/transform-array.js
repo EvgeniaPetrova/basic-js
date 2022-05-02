47a04b750dbeb7d2eac70a96e6d661a861467a4f
EvgeniaPetrova <ennkaaa@yandex.ru>
2022-05-03 01:32:28 +0300

    complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,31 +14,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  if (arr.length == 0) {
-    return [] }
   if (!Array.isArray(arr)) {
-    throw new Error (`'arr' parameter must be an instance of the Array!`)
-    }
+    throw new Error ("'arr' parameter must be an instance of the Array!")
+    };
+  if (arr.length == 0) {
+    return [] };
   if (Array.isArray(arr)) {
     let newArr = Object.assign([], arr);
     for(let i = 0; i < arr.length; i++ ) {
     switch(arr[i]) {
       case '--discard-next':
-        if (!arr[arr.length - 1]) {
+        if (arr[i] !== arr[arr.length - 1]) {
         newArr.splice(i, 2);
-        return newArr;}
+        return newArr;
+      } else {
+        newArr.splice(i, 1);
+        return newArr;
+         }
       case '--discard-prev':
-        if (!arr[0]) {
+        if (arr[i] !== arr[0]) {
         newArr.splice(i-1, 2);
-        return newArr;}
+        return newArr;
+      }  else {
+        newArr.splice(i, 1);
+        return newArr;
+        }
       case '--double-next':
-        if (!arr[arr.length - 1]) {
+        if (arr[i] !== arr[arr.length - 1]) {
         newArr.splice(i, 1, newArr[i+1]);
-        return newArr;}
+        return newArr;
+      }  else {
+        newArr.splice(i, 1);
+        return newArr;
+        }
       case '--double-prev':  
-      if (!arr[0]) {
+      if (arr[i] !== arr[0]) {
       newArr.splice(i, 1, newArr[i-1]);
-      return newArr;}
+      return newArr;
+    }  else {
+      newArr.splice(i, 1);
+      return newArr;
+      }
     }
 } 
 } return arr;
